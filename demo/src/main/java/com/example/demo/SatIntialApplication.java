d9ca8f2fd8fb259d53624a722a3850b3d3745810
Reggy Gaines <reggy.gaines@gmail.com>
2024-10-07 11:39:33 -0400

    Initialize

---
+++ demo/src/main/java/com/example/demo/SatIntialApplication.java
@@ -0,0 +1,13 @@
+package demo.src.main.java.com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SatIntialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SatIntialApplication.class, args);
+	}
+
+}
---
+++ demo/src/test/java/com/example/demo/SatIntialApplicationTests.java
@@ -0,0 +1,13 @@
+package demo.src.test.java.com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SatIntialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
